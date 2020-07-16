789296e1d715c7d14bcb8b9c5a23424665220a4e
Gunar Ortiz <gunarortiz@gmail.com>
2020-07-16 19:59:21 -0400

    ultimos cambios

--- src/sumaresta/SumaResta.java
+++ src/sumaresta/SumaResta.java
@@ -5,6 +5,9 @@
  */
 package sumaresta;
 
+import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,6 +19,9 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 /**
  *
@@ -33,17 +39,14 @@ public class SumaResta extends Application {
         grid.setPadding(new Insets(10, 10, 10, 10));
         
         
-        Text txt1 = new Text("Numero 1");
+        Text txt1 = new Text("Ingrese la operacion, Ej: 1+2+3");
         grid.add(txt1, 0, 0);
         
-        TextField text1 = new TextField();
-        grid.add(text1, 1, 0);
-        
-        Text txt2 = new Text("Numero 2");
+        Text txt2 = new Text("Numero 1");
         grid.add(txt2, 0, 1);
         
-        TextField text2 = new TextField();
-        grid.add(text2, 1, 1);
+        TextField text1 = new TextField();
+        grid.add(text1, 1, 1);
         
         Text txt3 = new Text("");
         grid.add(txt3, 0, 4);
@@ -54,51 +57,20 @@ public class SumaResta extends Application {
             
             @Override
             public void handle(ActionEvent event) {
+                String numero1= text1.getText();
                 
-                int suma = Integer.parseInt(text1.getText()) + Integer.parseInt(text2.getText());
-                txt3.setText("Suma Infija\n el resultado es: " +suma);
-                System.out.println("El resultado es: " +suma);
+                ScriptEngineManager mgr = new ScriptEngineManager();
+                ScriptEngine engine = mgr.getEngineByName("JavaScript");
+                String foo = numero1;
+                try {
+                    txt3.setText("El resultado es:\nSuma Infija: " + numero1 + " = " + engine.eval(foo)+"\n Suma Prefija: "+ infixToPrefix(numero1) + " = " + engine.eval(foo));
+                } catch (ScriptException ex) {
+                    Logger.getLogger(SumaResta.class.getName()).log(Level.SEVERE, null, ex);
+                }
+              
             }
         });
         grid.add(btn, 0, 2);
-        
-        
-        
-        Button btn1 = new Button();
-        btn1.setText("Suma Prefija");
-        btn1.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                
-                int suma = Integer.parseInt(text1.getText()) + Integer.parseInt(text2.getText());
-                txt3.setText("Suma Prefija, el resultado es: " +suma);
-                System.out.println("El resultado es: " +suma);
-            }
-        });
-        grid.add(btn1, 1, 2);
-        
-        
-        
-        
-        
-        Button btn2 = new Button();
-        btn2.setText("Suma Sufija");
-        btn2.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                
-                int suma = Integer.parseInt(text1.getText()) + Integer.parseInt(text2.getText());
-                txt3.setText("Suma Sufija, el resultado es: " +suma);
-                System.out.println("El resultado es: " +suma);
-            }
-        });
-        grid.add(btn2, 0, 3);
-        
-        
-        
-        
         
         Scene scene = new Scene(grid, 300, 250);
         
@@ -114,4 +86,108 @@ public class SumaResta extends Application {
         launch(args);
     }
     
+    
+    
+    
+    static boolean isOperator(char c) 
+{ 
+    return (!(c >= 'a' && c <= 'z') &&  
+            !(c >= '0' && c <= '9') &&  
+            !(c >= 'A' && c <= 'Z')); 
+} 
+  
+static int getPriority(char C) 
+{ 
+    if (C == '-' || C == '+') 
+        return 1; 
+    else if (C == '*' || C == '/') 
+        return 2; 
+    else if (C == '^') 
+        return 3; 
+    return 0; 
+} 
+
+
+static String infixToPrefix(String infix) 
+{ 
+    Stack<Character> operators = new Stack<Character>(); 
+  
+    Stack<String> operands = new Stack<String>(); 
+  
+    for (int i = 0; i < infix.length(); i++)  
+    { 
+  
+        if (infix.charAt(i) == '(')  
+        { 
+            operators.push(infix.charAt(i)); 
+        } 
+  
+        else if (infix.charAt(i) == ')')  
+        { 
+            while (!operators.empty() &&  
+                operators.peek() != '(')  
+                { 
+  
+                String op1 = operands.peek(); 
+                operands.pop(); 
+
+                String op2 = operands.peek(); 
+                operands.pop(); 
+  
+                char op = operators.peek(); 
+                operators.pop(); 
+   
+                String tmp = op + op2 + op1; 
+                operands.push(tmp); 
+            } 
+  
+            operators.pop(); 
+        } 
+  
+        else if (!isOperator(infix.charAt(i)))  
+        { 
+            operands.push(infix.charAt(i) + ""); 
+        } 
+        else 
+        { 
+            while (!operators.empty() &&  
+                getPriority(infix.charAt(i)) <=  
+                    getPriority(operators.peek()))  
+                { 
+  
+                String op1 = operands.peek(); 
+                operands.pop(); 
+  
+                String op2 = operands.peek(); 
+                operands.pop(); 
+  
+                char op = operators.peek(); 
+                operators.pop(); 
+  
+                String tmp = op + op2 + op1; 
+                operands.push(tmp); 
+            } 
+  
+            operators.push(infix.charAt(i)); 
+        } 
+    } 
+  
+    while (!operators.empty())  
+    { 
+        String op1 = operands.peek(); 
+        operands.pop(); 
+  
+        String op2 = operands.peek(); 
+        operands.pop(); 
+  
+        char op = operators.peek(); 
+        operators.pop(); 
+  
+        String tmp = op + op2 + op1; 
+        operands.push(tmp); 
+    } 
+  
+    return operands.peek(); 
+}
+
 }
